8db9d74a16cc75fca40525eb101e881cb472c40a
aliko <mr.alikoci@gmail.com>
2024-05-29 22:25:55 +0300

    searchdiv autocomplete

--- resources/js/Pages/Home/Partials/SearchDiv.tsx
+++ resources/js/Pages/Home/Partials/SearchDiv.tsx
@@ -27,6 +27,7 @@ const SearchDiv = () => {
                         name="search"
                         className="w-full sm:w-2/3 py-3 px-4 !ring-0 !outline-none !rounded-l-xl !rounded-r-none !border-none focus:!border-none dark:!bg-lotus-800 dark:placeholder:text-white focus:placeholder:text-transparent dark:focus:placeholder:text-transparent"
                         placeholder="Aklından neler geçiyor ?"
+                        autoComplete="off"
                     />
                     <button className="p-3 bg-white text-royal-600 dark:text-FFF2D7 dark:!bg-lotus-800 border-l-2 rounded-r-xl dark:border-FFF2D7/50 hover:text-royal-950 dark:hover:text-lotus-500">
                         <IoSearchSharp className="w-6 h-6" />
