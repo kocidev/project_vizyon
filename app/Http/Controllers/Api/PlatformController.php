ae9b62300f5542c013cc361d27732b3d30f1eff9
aliko <mr.alikoci@gmail.com>
2024-05-31 21:11:52 +0300

    fix

--- app/Http/Controllers/Api/PlatformController.php
+++ app/Http/Controllers/Api/PlatformController.php
@@ -15,21 +15,21 @@ class PlatformController extends Controller
                     'id' => 1,
                     'name' => 'Stranger Things',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
                 [
                     'id' => 2,
                     'name' => 'The Witcher',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
                 [
                     'id' => 3,
                     'name' => 'Bird Box',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
             ],
@@ -38,21 +38,21 @@ class PlatformController extends Controller
                     'id' => 1,
                     'name' => 'Stranger Things',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
                 [
                     'id' => 2,
                     'name' => 'The Witcher',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
                 [
                     'id' => 3,
                     'name' => 'Bird Box',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
             ],
@@ -61,21 +61,21 @@ class PlatformController extends Controller
                     'id' => 1,
                     'name' => 'The Mandalorian',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
                 [
                     'id' => 2,
                     'name' => 'WandaVision',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
                 [
                     'id' => 3,
                     'name' => 'Soul',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
             ],
@@ -84,21 +84,21 @@ class PlatformController extends Controller
                     'id' => 1,
                     'name' => 'Ted Lasso',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
                 [
                     'id' => 2,
                     'name' => 'The Morning Show',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
                 [
                     'id' => 3,
                     'name' => 'Greyhound',
                     'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e2499817c466504601f8619bd55d268645b3a614b4f2fecc17d77eafdc1c2857._RI_TTW_SX1080_FMjpg_.jpg',
+                    'image' => 'https://picsum.photos/300/180',
                     'type' => 'Series'
                 ],
             ]
