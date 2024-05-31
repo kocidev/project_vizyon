6894be90f888fd6a31c8c0a6e9642c9c1245211d
aliko <mr.alikoci@gmail.com>
2024-05-31 21:12:15 +0300

    UpComing

---
+++ resources/js/Pages/Home/Partials/UpComing.tsx
@@ -0,0 +1,13 @@
+const UpComing = () => {
+    return (
+        <>
+            <div className="px-2 sm:px-0 mt-4 sm:mt-6 mb-4">
+                <h1 className="text-royal-950 dark:text-FFF2D7 drop-shadow-sm font-extrabold text-2xl sm:text-3xl">
+                    Gelecek Filmler
+                </h1>
+            </div>
+        </>
+    );
+};
+
+export default UpComing;
