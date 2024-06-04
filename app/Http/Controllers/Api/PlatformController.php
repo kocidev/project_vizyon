441b8c08da0b58cd6895afdd4d45736fe1a7d59b
aliko <mr.alikoci@gmail.com>
2024-06-04 22:02:43 +0300

    ana sayfada bulunan platform kısmı tamamlandı

--- app/Http/Controllers/Api/PlatformController.php
+++ app/Http/Controllers/Api/PlatformController.php
@@ -3,109 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\StreamingAvailabilityService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class PlatformController extends Controller
 {
-    public function getPlatformContent(string $platform): JsonResponse
+    protected $streamingAvailabilityService;
+
+    public function __construct(StreamingAvailabilityService $streamingAvailabilityService)
     {
-        $fakeData = [
-            'amazon' => [
-                [
-                    'id' => 1,
-                    'name' => 'Stranger Things',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-                [
-                    'id' => 2,
-                    'name' => 'The Witcher',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-                [
-                    'id' => 3,
-                    'name' => 'Bird Box',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-            ],
-            'netflix' => [
-                [
-                    'id' => 1,
-                    'name' => 'Stranger Things',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-                [
-                    'id' => 2,
-                    'name' => 'The Witcher',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-                [
-                    'id' => 3,
-                    'name' => 'Bird Box',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-            ],
-            'disney' => [
-                [
-                    'id' => 1,
-                    'name' => 'The Mandalorian',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-                [
-                    'id' => 2,
-                    'name' => 'WandaVision',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-                [
-                    'id' => 3,
-                    'name' => 'Soul',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-            ],
-            'apple' => [
-                [
-                    'id' => 1,
-                    'name' => 'Ted Lasso',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-                [
-                    'id' => 2,
-                    'name' => 'The Morning Show',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-                [
-                    'id' => 3,
-                    'name' => 'Greyhound',
-                    'description' => 'Lorem ipsum sipsum.',
-                    'image' => 'https://picsum.photos/300/180',
-                    'type' => 'Series'
-                ],
-            ]
-        ];
+        $this->streamingAvailabilityService = $streamingAvailabilityService;
+    }
 
-        $content = $fakeData[strtolower($platform)] ?? [];
+    public function getPlatformPopular(string $platform, Request $request): JsonResponse
+    {
+        $request->validate([
+            'cursor' => 'nullable|string',
+        ]);
+        $cursor = $request->query('cursor');
 
-        return response()->json($content);
+        $result = $this->streamingAvailabilityService->getPlatformPopularShows($platform, $cursor);
+        if ($result->isSuccess) {
+            return response()->json($result->data);
+        } else {
+            return response()->json(['error' => $result->error], 500);
+        }
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\StreamingAvailabilityService;
 use App\Services\TmdbService;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -9,23 +10,28 @@ use Inertia\Response;
 class HomeController extends Controller
 {
     protected $tmdbService;
+    protected $streamingAvailabilityService;
 
-    public function __construct(TmdbService $tmdbService)
+    public function __construct(TmdbService $tmdbService, StreamingAvailabilityService $streamingAvailabilityService)
     {
         $this->tmdbService = $tmdbService;
+        $this->streamingAvailabilityService = $streamingAvailabilityService;
     }
 
     public function index(): Response
     {
         $theaters = $this->tmdbService->getMovieNowPlaying(1);
         $upComings = $this->tmdbService->getMovieUpComing(1);
+        $platform = $this->streamingAvailabilityService->getPlatformPopularShows("prime");
 
         $theatersData = $theaters->isSuccess ? $theaters->data : [];
         $upComingsData = $upComings->isSuccess ? $upComings->data : [];
+        $platformData = $platform->isSuccess ? $platform->data : [];
 
         return Inertia::render('Home/index', [
             'theaters' => $theatersData,
             'upComings' => $upComingsData,
+            'platform' => $platformData,
         ]);
     }
 }
