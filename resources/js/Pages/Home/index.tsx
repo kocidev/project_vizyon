371153acea4de514bfe48dcf85396050458628e7
aliko <mr.alikoci@gmail.com>
2024-06-04 22:03:18 +0300

    platformcontents

--- resources/js/Pages/Home/index.tsx
+++ resources/js/Pages/Home/index.tsx
@@ -1,17 +1,19 @@
 import { PageProps } from "@/types";
-import { iMovie } from "@/types";
+import { iMovie } from "@/types/movie.type";
 import CoreLayout from "@/Layouts/Core";
 import Theaters from "@/Pages/Home/Partials/Theaters";
 import SearchDiv from "@/Pages/Home/Partials/SearchDiv";
 import PlatformContents from "@/Pages/Home/Partials/PlatformContents";
 import MovieUpComing from "./Partials/MovieUpComing";
+import { iPlatform } from "@/types/platform.type";
 
 interface HomeProps extends PageProps {
     theaters: iMovie[];
     upComings: iMovie[];
+    platform: iPlatform;
 }
 
-const Home = ({ auth, theaters, upComings }: HomeProps) => {
+const Home = ({ auth, theaters, upComings, platform }: HomeProps) => {
     return (
         <>
             <CoreLayout user={auth.user} title="Anasayfa">
@@ -25,7 +27,7 @@ const Home = ({ auth, theaters, upComings }: HomeProps) => {
                     <MovieUpComing upComings={upComings} />
                 </section>
                 <section id="platform-contents">
-                    <PlatformContents />
+                    <PlatformContents platform={platform} />
                 </section>
             </CoreLayout>
         </>
